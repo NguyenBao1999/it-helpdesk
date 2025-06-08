ef21192e7fa592f719a6f2d6d60b76e278207b58
NguyenBao1999 <nguyenlehoangbao9991@gmail.com>
2025-06-08 11:36:31 +0700

    get-ip-config

--- scripts/get-ip-config.ps1
+++ scripts/get-ip-config.ps1
@@ -5,3 +5,9 @@ Write-Output "Đang lấy thông tin IP..."
 Get-NetIPAddress |
     Where-Object { $_.AddressFamily -eq "IPv4" } |
     Format-Table InterfaceAlias, IPAddress, PrefixLength
+
+# Chỉ lấy IP khi kết nối Wifi
+
+Get-NetIPAddress -InterfaceAlias "Wi-Fi" |
+  Where-Object { $_.AddressFamily -eq "IPv4" } |
+  Format-Table InterfaceAlias, IPAddress, PrefixLength
